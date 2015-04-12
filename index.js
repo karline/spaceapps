d716aaa6325e4a13ba39930741687fb5b36eeea3
Andre Behrens <andre@rallypoint.com>
2015-04-12 10:18:42 -0400

    First efforts to hook up backend.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var app = express();
 app.use("/assets", express.static(__dirname + '/assets'));
 app.use("/bootstrap", express.static(__dirname + '/bootstrap'));
@@ -7,6 +8,19 @@ app.use("/custom.css", express.static(__dirname + '/custom.css'));
 
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: __dirname })
+});
+
+app.post('/submit-data', function(req, res) {
+  var remoteUrl = 'http://107.170.101.126:8080';
+  fetch(remoteUrl, {method: post})
+    .then(function(resp) {
+      console.log('resp!');
+      console.log(resp);
+      return resp.json();
+    }).then(function(json) {
+      console.log('json!');
+      console.log(json);
+    });
 });
 
 app.get('/about.html', function(req, res) {
